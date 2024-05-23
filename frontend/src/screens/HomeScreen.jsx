842405a1785daf68420e4ddbd994effd0fd1735c
Cédric goubil <cedricgoubil.zh@gmail.com>
2024-05-23 19:18:00 +0200

    Cart

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,14 +1,19 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
   return (
     <>
       { isLoading ? (
-        <h2>Loading...</h2>
-      ) : error ? (<div>{ error?.data?.message || error.error}</div>) : (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+        { error?.data?.message || error.error}
+        </Message>) : (
         <>
         <h1>Latest Products</h1>
           <Row>
