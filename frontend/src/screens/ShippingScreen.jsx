37b29a5a18d551b700f4e6ba4d81dadc05aa91c8
Cédric goubil <cedricgoubil.zh@gmail.com>
2024-08-02 13:48:00 +0200

    Implementing checkoutsteps

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -3,8 +3,9 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
-
 import { saveShippingAddress } from '../slices/cartSlice';
+import CheckoutSteps  from '../components/CheckoutSteps';
+
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
@@ -32,6 +33,7 @@ const ShippingScreen = () => {
   return (
     <FormContainer>
       <h1>Shipping</h1>
+      <CheckoutSteps step1 step2/>
       <Form onSubmit={submitHandler}>
         <Form.Group className='my-2' controlId='address'>
           <Form.Label>Address</Form.Label>
