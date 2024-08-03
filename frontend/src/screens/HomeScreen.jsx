46a3cf449aaf51159f3f3c5a1f40acdc6013c2c7
Cédric goubil <cedricgoubil.zh@gmail.com>
2024-08-03 16:46:16 +0200

    Adding meta

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,6 +6,7 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 import Paginate from '../components/Paginate';
 import ProductCarousel from '../components/ProductCarousel';
+import Meta from '../components/Meta';
 
 const HomeScreen = () => {
   const { pageNumber, keyword } = useParams();
@@ -30,6 +31,7 @@ const HomeScreen = () => {
         { error?.data?.message || error.error}
         </Message>) : (
         <>
+        <Meta />
         <h1>Latest Products</h1>
           <Row>
           { data.products.map((product) => (
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -9,6 +9,7 @@ import { useGetProductDetailsQuery, useCreateReviewMutation } from '../slices/pr
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { addToCart } from '../slices/cartSlice';
+import Meta from '../components/Meta';
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
@@ -63,6 +64,7 @@ const ProductScreen = () => {
           { error?.data?.message || error.error}
           </Message>) : (
           <>
+           <Meta title={product.name}/>
               <Row>
                 <Col md={5}>
                   <Image src={product.image} alt={product.name} fluid/>
