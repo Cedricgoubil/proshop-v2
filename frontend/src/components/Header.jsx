12c7ca89a0c6fa652d330966a2c02e85b057fe27
Cédric goubil <cedricgoubil.zh@gmail.com>
2024-08-03 16:26:47 +0200

    SearchBox

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,6 +6,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import SearchBox from './SearchBox';
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -37,17 +38,18 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className='ms-auto'>
-                <LinkContainer to='/cart'>
-                  <Nav.Link >
-                    <FaShoppingCart /> Cart
-                    {cartItems.length > 0 && (
-                      <Badge pill bg='success' style={{ marginLeft: '5px' }}>
-                        {cartItems.reduce((a, c) => a + c.qty, 0)}
-                      </Badge>
-                    )}
-                  </Nav.Link>
-                </LinkContainer>
-                {userInfo ? (
+                <SearchBox />
+                  <LinkContainer to='/cart'>
+                    <Nav.Link >
+                      <FaShoppingCart /> Cart
+                      {cartItems.length > 0 && (
+                        <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                          {cartItems.reduce((a, c) => a + c.qty, 0)}
+                        </Badge>
+                      )}
+                    </Nav.Link>
+                  </LinkContainer>
+                  {userInfo ? (
                 <>
                   <NavDropdown title={userInfo.name} id='username'>
                     <NavDropdown.Item as={Link} to='/profile'>
@@ -58,25 +60,25 @@ const Header = () => {
                     </NavDropdown.Item>
                   </NavDropdown>
                 </>
-              ) : (
-                <Nav.Link as={Link} to='/login'>
-                  <FaUser /> Sign In
-                </Nav.Link>
-              )}
-              {/* Admin Links */}
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                  <NavDropdown.Item as={Link} to='/admin/productlist'>
-                    Products
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/admin/orderlist'>
-                    Orders
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/admin/userlist'>
-                    Users
-                  </NavDropdown.Item>
-                </NavDropdown>
-              )}
+                  ) : (
+                    <Nav.Link as={Link} to='/login'>
+                      <FaUser /> Sign In
+                    </Nav.Link>
+                  )}
+                  {/* Admin Links */}
+                  {userInfo && userInfo.isAdmin && (
+                    <NavDropdown title='Admin' id='adminmenu'>
+                      <NavDropdown.Item as={Link} to='/admin/productlist'>
+                        Products
+                      </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to='/admin/orderlist'>
+                        Orders
+                      </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to='/admin/userlist'>
+                        Users
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  )}
               </Nav>
             </Navbar.Collapse>
         </Container>
